5a2159fbd9a04a7dfda14336acd6146ae3cd63a1
guru <guruyewandge@gmail.com>
2025-04-07 02:18:25 +0530

    merge sorted array

---
+++ sorting_algorithms/merge.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int idx = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[idx--] = nums1[i--];
+            } else {
+                nums1[idx--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[idx--] = nums2[j--];
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3, n = 3;
+
+    sol.merge(nums1, m, nums2, n);
+
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
